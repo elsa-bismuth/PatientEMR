cbb1cc3a8e2c5f63f61fe6887b6f5d68d9dbaaaa
elsa-bismuth <elsa.bismuth@polytechnique.edu>
2025-01-12 17:28:20 -0800

    Initial Commit

---
+++ PatientEMR - A1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PatientEMR - A1
+//
+//  Created by Annabel Bismuth on 12/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
